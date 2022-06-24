7d657b9bd357513b43d1714f37e442c461836d48
hardik <hardickupadhyay008@gmail.com>
2022-06-24 13:12:12 +0530

    two

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,7 +5,6 @@ const blogsController = require("../controllers/blogsController");
 const authenticateMW = require("../middlewares/authenticateMid");
 const authorizeMW = require("../middlewares/authorizeMid");
 
-<<<<<<< HEAD
 // 1)
 router.post("/createAuthor", authorController.createAuthor);
 
@@ -40,21 +39,7 @@ router.delete(
 );
 
 module.exports = router;
-=======
-/// controllers///
-const authorController = require("../controllers/authorController")
-const blogController = require("../controllers/blogController")
-const middleware=  require("../middlewares/commonMiddlewares")
 
-/// Requested APIs///
-router.post("/authors", authorController.createAuthor)
-router.post("/blogs",middleware.mid1,middleware.mid2,blogController.createBlog)
-router.get("/blogs",middleware.mid1,blogController.getBlogs)
-router.put("/blogs/:blogId",middleware.mid1,middleware.mid2, blogController.updateBlog)
-router.delete('/blogs/:blogId',middleware.mid1,middleware.mid2,blogController.deleteBlog)
-router.delete('/blogs',middleware.mid1,blogController.blogDeleteQuery)
-router.post("/login", authorController.logInAuthor)
 
 ///routers ///
 module.exports = router;
->>>>>>> 44e8884cec84aa773f59eb99310dccabcd5cabf0
